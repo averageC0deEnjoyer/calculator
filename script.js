6f79b9448b76be3340239b99f554f49ad28bbc6a
averageC0deEnjoyer <136006438+averageC0deEnjoyer@users.noreply.github.com>
2023-09-20 16:41:17 +0700

    add all number button and all operator

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 let firstNumber = "";
 let secondNumber = "";
-let operator = '';
-let storeResult = 0;
+let operator = "";
+let storeResult = "";
 
 
 //initialize all button
 clearBtn = document.querySelector('.clear.button');
 deleteBtn = document.querySelector('.delete.button');
+zeroBtn = document.querySelector('.zero.button');
 oneBtn = document.querySelector('.one');
 twoBtn = document.querySelector('.two.button');
 threeBtn = document.querySelector('.three.button');
@@ -25,41 +26,68 @@ resultDisplay = document.querySelector('.result');
 calculation = document.querySelector('.calculation');
 equalBtn = document.querySelector('.equal.button');
 resultBool = false; //create a value to check if result already in display, then need to clearDisplay();
+allNumberBtn = [zeroBtn, oneBtn, twoBtn, threeBtn, fourBtn, fiveBtn, sixBtn, sevenBtn, eightBtn, nineBtn];
+operatorBtn = [addBtn, substractBtn, multiplyBtn, divideBtn];
 
-oneBtn.addEventListener('click',(event) => {
-    let element = event.target;
-    if(resultBool){
-        resultDisplay.textContent = '';
-    }
-    if(operator === ''){
-        resultDisplay.textContent += element.value;
-        firstNumber += element.value;
-        console.log(firstNumber);
-        // calculation.textContent += element.value;
-    } else {
-        if(resultDisplay.textContent.includes(operator)){
+allNumberBtn.forEach(button => {
+    button.addEventListener('click',(event) => {
+        let element = event.target;
+        if(resultBool){
             resultDisplay.textContent = '';
+            resultBool = false;
         }
-        resultDisplay.textContent += element.value;
-        secondNumber += element.value;
-        // calculation.textContent += element.value;
-        console.log(secondNumber)
-    }
+        if(operator === ''){
+            resultDisplay.textContent += element.value;
+            firstNumber += element.value;
+            console.log(firstNumber);
+            // calculation.textContent += element.value;
+        } else {
+            if(resultDisplay.textContent.includes(operator)){
+                resultDisplay.textContent = '';
+            }
+            resultDisplay.textContent += element.value;
+            secondNumber += element.value;
+            // calculation.textContent += element.value;
+            console.log(secondNumber)
+        }
+    })
 })
 
-addBtn.addEventListener('click',(event) => {
+operatorBtn.forEach(button => {
+    button.addEventListener('click',(event) => {
     let element = event.target;
     operator = element.value;
     console.log(operator);
     resultDisplay.textContent = operator;
-
+    })
 })
 
 equalBtn.addEventListener('click', (event) => {
-    if(operator == '+'){
+    if (operator == '+') {
         parseFirstNumber = parseInt(firstNumber);
         parseSecondNumber = parseInt(secondNumber);
         let result = add(parseFirstNumber,parseSecondNumber);
+        resultDisplay.textContent = result;
+        storeResult = result;
+        resultBool = true;
+    } else if (operator == '-') {
+        parseFirstNumber = parseInt(firstNumber);
+        parseSecondNumber = parseInt(secondNumber);
+        let result = substract(parseFirstNumber,parseSecondNumber);
+        resultDisplay.textContent = result;
+        storeResult = result;
+        resultBool = true;
+    } else if (operator == '*') {
+        parseFirstNumber = parseInt(firstNumber);
+        parseSecondNumber = parseInt(secondNumber);
+        let result = multiply(parseFirstNumber,parseSecondNumber);
+        resultDisplay.textContent = result;
+        storeResult = result;
+        resultBool = true;
+    } else if (operator == '+') {
+        parseFirstNumber = parseInt(firstNumber);
+        parseSecondNumber = parseInt(secondNumber);
+        let result = divide(parseFirstNumber,parseSecondNumber);
         resultDisplay.textContent = result;
         storeResult = result;
         resultBool = true;
@@ -68,20 +96,25 @@ equalBtn.addEventListener('click', (event) => {
 
 
 deleteBtn.addEventListener('click', ()=> {
-    if(firstNumber.length>0 && secondNumber.length == 0) {
+    if(firstNumber.length > 0 && secondNumber.length == 0 & !resultBool) {
         let slicedFirstNumber = firstNumber.slice(0,-1);
         resultDisplay.textContent = slicedFirstNumber;
         firstNumber = slicedFirstNumber;
-    } else if (firstNumber.length != 0 && secondNumber.length > 0){
+    } else if (firstNumber.length != 0 && secondNumber.length > 0 & !resultBool){
         let slicedSecondNumber = secondNumber.slice(0,-1);
         resultDisplay.textContent = slicedSecondNumber;
         secondNumber = slicedSecondNumber;
-    }        
+    } else if (resultBool) {
+        slicedResult = storeResult.toString().slice(0,-1);
+        resultDisplay.textContent = slicedResult;
+        storeResult = slicedResult;
+        console.log(slicedResult);
+    }       
 })
 
-
-twoBtn.addEventListener('click',(event) => {
-    firstNumber = event.value;
+//clear everything
+clearBtn.addEventListener('click', () => {
+    clearDisplay();
 })
 
 
@@ -120,5 +153,9 @@ function operate(firstNumber, secondNumber, operator){
 }
 
 function clearDisplay(){
-    resultDisplay.textContent = '';
+    resultDisplay.textContent = ''
+    firstNumber = "";
+    secondNumber = "";
+    operator = '';
+    storeResult = "";
 }
